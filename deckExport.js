cdac75e09b5218440bfdbcb4eac414f1cdb9d941
Hung Nguyen <165174644+HungNguyen922@users.noreply.github.com>
2025-08-28 12:40:34 -0500

    exporting a deck should save it as custom name for the deck

--- deckExport.js
+++ deckExport.js
@@ -1,5 +1,6 @@
 const downloadBtn = document.getElementById("download-deck");
 const deckExportWrapper = document.getElementById("deck-export-wrapper");
+const deckTitle = document.getElementById("deck-title");
 
 downloadBtn.addEventListener("click", () => {
     // Use html2canvas to capture the deck area
@@ -8,7 +9,7 @@ downloadBtn.addEventListener("click", () => {
         scale: 5 // optional: higher resolution
     }).then(canvas => {
         const link = document.createElement("a");
-        link.download = "my-deck.png";
+        link.download = deck.Title + '.png';
         link.href = canvas.toDataURL("image/png");
         link.click();
     }).catch(err => console.error("Error generating deck image:", err));
