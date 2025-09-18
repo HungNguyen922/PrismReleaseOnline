789d9005a7761ef2224cb4b25e2f5517e6f9ec6f
Hung Nguyen <165174644+HungNguyen922@users.noreply.github.com>
2025-09-17 23:33:18 -0500

    Update cardMaker.js

--- cardMaker.js
+++ cardMaker.js
@@ -127,8 +127,8 @@ function drawCard() {
     document.getElementById("line3").value
   ];
   
-  const textYStart = 600;   // adjust based on your card size
-  const lineHeight = 75;    // space between lines
+  const textYStart = 650;   // adjust based on your card size
+  const lineHeight = 100;    // space between lines
   const maxTextWidth = 500; // maximum width before wrapping
   
   effectLines.forEach((line, i) => {
