007c420947a73b4bc0ca6d6cda4cd958bba2d300
Hung Nguyen <165174644+HungNguyen922@users.noreply.github.com>
2025-09-17 20:11:02 -0500

    Update cardMaker.js

--- cardMaker.js
+++ cardMaker.js
@@ -89,10 +89,10 @@ function drawCard() {
   ctx.textAlign = "center";
   ctx.lineWidth = 10;                // thickness of the outline
   ctx.strokeStyle = "white";        // outline color
-  ctx.strokeText(power, 65, 85);
-  ctx.fillText(power, 65, 85);
-  ctx.strokeText(bulk, 65, 165);
-  ctx.fillText(bulk, 65, 165);
+  ctx.strokeText(power, 65, 90);
+  ctx.fillText(power, 65, 90);
+  ctx.strokeText(bulk, 65, 170);
+  ctx.fillText(bulk, 65, 170);
 
   // color icons
   const colors = [
